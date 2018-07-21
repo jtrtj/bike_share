c3667325754ffe23b945af105ca2e6a9984db579
jtrtj <jt.r@live.com>
2018-07-21 16:17:05 -0600

    add time.now objects to test and now it's green

--- spec/features/conditions/visitor_sees_conditions_index_spec.rb
+++ spec/features/conditions/visitor_sees_conditions_index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'A visitor' do
   context 'visits conditions index' do
     it 'sees all conditions with Date, Max Temperature, Mean Temperature, Min Temperature, Mean Humidity, Mean Visibility (in Miles), Mean Wind Speed (mph), Precipitation (inches)' do
-      condition_1 = Condition.create!(date: 2018-03-12, max_temperature_f: 90, mean_temperature_f: 80, min_temperature_f: 70, mean_humidity: 60, mean_visibility_miles: 70, mean_wind_speed_mph: 30, precipitation_inches: 25)
-      condition_2 = Condition.create(date: 2018-03-12, max_temperature_f: 80, mean_temperature_f: 70, min_temperature_f: 60, mean_humidity: 60, mean_visibility_miles: 30, mean_wind_speed_mph: 20, precipitation_inches: 2)
+      condition_1 = Condition.create!(date: Time.now, max_temperature_f: 90.0, mean_temperature_f: 80.0, min_temperature_f: 70.0, mean_humidity: 60.0, mean_visibility_miles: 70.0, mean_wind_speed_mph: 30.0, precipitation_inches: 25.0)
+      condition_2 = Condition.create(date: Time.now, max_temperature_f: 80.0, mean_temperature_f: 70.0, min_temperature_f: 60.0, mean_humidity: 60.0, mean_visibility_miles: 30.0, mean_wind_speed_mph: 20.0, precipitation_inches: 2.0)
 
       visit conditions_path
      
