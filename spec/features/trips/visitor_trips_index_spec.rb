9c1456c1f29551e6cd796f21b1d2d4e46501fca0
Eliot Swank <eliotswank@gmail.com>
2018-07-21 16:29:25 -0600

    adds instance var to trips index method and corrects index view

--- spec/features/trips/visitor_trips_index_spec.rb
+++ spec/features/trips/visitor_trips_index_spec.rb
@@ -3,17 +3,15 @@ require 'rails_helper'
 describe "A visitor" do
   context "visits trips index page" do
     it "sees duration, start date, start station, end date, end station, bike id, subscription type, zip code of a trip" do
-      station1 = create(:station)
-      station2 = create(:station)
       trip = create(:trip)
 
       visit trips_path
-
+save_and_open_page
       expect(page).to have_content(trip.duration) 
       expect(page).to have_content(trip.start_date) 
-      expect(page).to have_content(trip.start_station) 
+      expect(page).to have_content(trip.start_station_id) 
       expect(page).to have_content(trip.end_date) 
-      expect(page).to have_content(trip.end_station) 
+      expect(page).to have_content(trip.end_station_id) 
       expect(page).to have_content(trip.bike_id) 
       expect(page).to have_content(trip.subscription_type)
       expect(page).to have_content(trip.zip_code)
