a0431c39452208f0bfe7382718fd8766663273a4
jtrtj <jt.r@live.com>
2018-07-21 14:21:18 -0600

    add stations to routes

--- spec/features/stations/user_visits_station_index_spec.rb
+++ spec/features/stations/user_visits_station_index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'A visitor' do
-  scenario 'visits stations index' do
+  context 'visits stations index' do
     it 'sees all stations with name, dock count, city, installation date' do
       station_1 = Station.create(name: 'City hall', dock_count: 20, city: 'San Jose', installation_date: 2018-02-21, created_at: 2018-02-21, updated_at: 2018-03-21)
       station_2 = Station.create(name: 'South Blvd', dock_count: 15, city: 'San Jose', installation_date: 2018-01-11, created_at: 2018-01-11, updated_at: 2018-02-11)
