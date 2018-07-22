6513f224d605a04d73e82e68e2ccad6e4826be5e
Eliot Swank <eliotswank@gmail.com>
2018-07-22 13:52:10 -0600

    adds feature test for trip show page

---
+++ spec/features/trips/visitor_sees_a_trip_show_page_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "A visitor" do
+  context "visits the trip show page" do
+    it "sees a list of all attributes of a trip" do
+      station = create(:station)
+      trip = create(:trip, start_station: station, end_station: station)
+
+      visit trip_path
+
+      expect(page).to have_content(trip.duration)
+      expect(page).to have_content(trip.start_date)
+      expect(page).to have_content(trip.start_station_name)
+      expect(page).to have_content(trip.start_station_id)
+      expect(page).to have_content(trip.end_date)
+      expect(page).to have_content(trip.end_station_name)
+      expect(page).to have_content(trip.end_station_id)
+      expect(page).to have_content(trip.bike_id)
+      expect(page).to have_content(trip.subscription_type)
+      expect(page).to have_content(trip.zip_code)
+    end
+  end
+end
+
+=begin
+As a visitor,
+When I visit the trip show,
+I see all attributes for that trip.
+=end
