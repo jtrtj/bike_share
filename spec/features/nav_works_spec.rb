a098295ebc9e7951f5a440b3d1aad3d6966c3010
Eliot Swank <eliotswank@gmail.com>
2018-07-22 10:17:41 -0600

    add test for nav logo redirect

--- spec/features/nav_works_spec.rb
+++ spec/features/nav_works_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe "nav system" do
-    #test links go in here as links become available
+   it 'goes to the root path when clicking the logo' do
+    visit trips_path
+
+    click_on "BikeShare"
+
+    expect(current_path).to eq(root_path)
+   end
 end
