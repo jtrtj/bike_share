447ea5ae0d3f9598f4ed151d17012221a339e0ed
Eliot Swank <eliotswank@gmail.com>
2018-07-22 15:17:45 -0600

    add trip info to page. working on styling

--- spec/features/trips/visitor_sees_a_trip_show_page_spec.rb
+++ spec/features/trips/visitor_sees_a_trip_show_page_spec.rb
@@ -4,11 +4,11 @@ describe "A visitor" do
   context "visits the trip show page" do
     it "sees a list of all attributes of a trip" do
       station = create(:station)
-      trip = create(:trip, start_station: station, end_station: station)
+      trip = create(:trip, start_station: station, end_station: station, duration: 120)
 
       visit trip_path(trip)
 save_and_open_page
-      expect(page).to have_content(trip.duration)
+      expect(page).to have_content(trip.duration / 60)
       expect(page).to have_content(trip.start_date)
       expect(page).to have_content(trip.start_station_name)
       expect(page).to have_content(trip.start_station_id)
