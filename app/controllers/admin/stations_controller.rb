2e45f1fd5f895788ef0acaedbf1cdef436b9672e
ClaireBvs <clairembeauvais@gmail.com>
2018-07-22 15:28:26 -0600

    add partial for flash notice

--- app/controllers/admin/stations_controller.rb
+++ app/controllers/admin/stations_controller.rb
@@ -2,8 +2,10 @@ class Admin::StationsController < Admin::BaseController
 
   def destroy
     station = Station.find(params[:id])
+    deleted_station_name = station.name
     station.destroy
 
+    flash[:notice] = "#{deleted_station_name} was deleted!"
     redirect_to stations_path
   end
 
