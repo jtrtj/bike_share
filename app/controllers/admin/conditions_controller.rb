2a74e7676e47ba77f0bfa363972575b3a4f22e55
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-23 14:09:17 -0600

    admin can create and edit a condition

--- app/controllers/admin/conditions_controller.rb
+++ app/controllers/admin/conditions_controller.rb
@@ -1,5 +1,20 @@
 class Admin::ConditionsController < Admin::BaseController
 
+  def new
+    @condition = Condition.new
+  end
+
+  def create
+    @condition = Condition.create(condition_params)
+    if @condition.save
+      flash[:notice] = "Condition for #{@condition.date} was created!"
+      redirect_to condition_path(@condition)
+    else
+      flash[:notice] = "Condition fields invalid"
+      redirect_to new_admin_condition_path
+    end
+  end
+
   def destroy
     condition = Condition.find(params[:id])
     deleted_date = condition.date
@@ -7,5 +22,10 @@ class Admin::ConditionsController < Admin::BaseController
 
     flash[:notice] = "Condition for #{deleted_date} was deleted!"
     redirect_to conditions_path
+  end
+
+  private
+  def condition_params
+    params.require(:condition).permit(:date, :max_temperature_f, :mean_temperature_f, :min_temperature_f, :mean_humidity, :mean_visibility_miles, :mean_wind_speed_mph, :precipitation_inches)
   end
 end
