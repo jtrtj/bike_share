28f5900037434f721e3a6f896845228024fd4fd2
ClaireBvs <clairembeauvais@gmail.com>
2018-07-23 14:24:10 -0600

    add class method average duration trip to Trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,9 @@ class Trip < ApplicationRecord
 
   belongs_to :start_station, class_name: 'Station', foreign_key: :start_station_id
   belongs_to :end_station, class_name: 'Station', foreign_key: :end_station_id
+
+  def self.average_duration_trip
+    average(:duration)
+  end
+  
 end
