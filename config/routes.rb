8baa9c45fcba0cdce9a2a4c35dd6ddcfe26ccfe9
Eliot Swank <eliotswank@gmail.com>
2018-07-23 12:19:19 -0600

    adds basic button syntax

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :stations, only: [:new, :create, :edit, :update, :destroy]
+    
   end
 
   resources :conditions, only: [:index, :show]
