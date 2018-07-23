60d0a1e2fff63dd51ce6ca400729ae8619582414
jtrtj <jt.r@live.com>
2018-07-23 16:17:36 -0600

    move the tests to station spec

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -11,4 +11,28 @@ describe Station, type: :model do
     it {should have_many(:statuses)}
     it {is_expected.to respond_to(:trips)}
   end
+
+  describe 'class methods' do
+    it '#station_with_most_rides_originating' do
+      user = create(:user)
+      station_1 = create(:station, name: 'Omlette')
+      station_2 = create(:station, name: 'Fromage')
+      trip = create(:trip, start_station: station_1, end_station: station_1, duration: 120)
+      trip2 = create(:trip, start_station: station_1, end_station: station_2, duration: 60)
+      trip3 = create(:trip, start_station: station_1, end_station: station_2, duration: 30)
+
+      expect(Trip.station_with_most_rides_originating).to eq(station_1)
+    end
+
+    it '#station_with_most_rides_ending' do
+      user = create(:user)
+      station_1 = create(:station, name: 'Omlette')
+      station_2 = create(:station, name: 'Fromage')
+      trip = create(:trip, start_station: station_1, end_station: station_1, duration: 120)
+      trip2 = create(:trip, start_station: station_1, end_station: station_2, duration: 60)
+      trip3 = create(:trip, start_station: station_1, end_station: station_2, duration: 30)
+
+      expect(Trip.station_with_most_rides_ending).to eq(station_2)
+    end
+  end
 end
