a4df3a79c6e08217982e5c6ee0c66cc6be36c47d
jtrtj <jt.r@live.com>
2018-07-23 11:59:45 -0600

    write test for admin condtions index

---
+++ spec/features/conditions/admin_visits_conditions_index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'an admin' do
+  context 'visiting conditions index' do
+    it 'will see all conditions and their info with an edit and delete button for each' do
+      condition_1 = create(:condition)
+      condition_2 = create(:condition)
+
+      visit conditions_path
+
+      within "#condition-#{condition_1.id}" do
+        expect(page).to have_button('Edit')
+        expect(page).to have_button('Delete')
+      end
+
+      within "#condition-#{condition_2.id}" do
+        expect(page).to have_button('Edit')
+        expect(page).to have_button('Delete')
+      end
+      
+      expect(page).to have_content(condition_1.date)
+      expect(page).to have_content(condition_1.max_temperature_f)
+      expect(page).to have_content(condition_1.mean_temperature_f)
+      expect(page).to have_content(condition_1.min_temperature_f)
+      expect(page).to have_content(condition_1.mean_humidity)
+      expect(page).to have_content(condition_1.mean_visibility_miles)
+      expect(page).to have_content(condition_1.mean_wind_speed_mph)
+      expect(page).to have_content(condition_1.precipitation_inches)
+
+      expect(page).to have_content(condition_2.date)
+      expect(page).to have_content(condition_2.max_temperature_f)
+      expect(page).to have_content(condition_2.mean_temperature_f)
+      expect(page).to have_content(condition_2.min_temperature_f)
+      expect(page).to have_content(condition_2.mean_humidity)
+      expect(page).to have_content(condition_2.mean_visibility_miles)
+      expect(page).to have_content(condition_2.mean_wind_speed_mph)
+      expect(page).to have_content(condition_2.precipitation_inches)
+    end
+  end
+end
