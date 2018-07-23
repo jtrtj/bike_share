09af723770c916873a48a242e6b1c9d5750746f5
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-23 12:15:49 -0600

    Added buttons to alter conditions for admin

--- spec/features/conditions/admin_visits_conditions_index_spec.rb
+++ spec/features/conditions/admin_visits_conditions_index_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 describe 'an admin' do
   context 'visiting conditions index' do
     it 'will see all conditions and their info with an edit and delete button for each' do
+      admin = create(:user, role: 1)
+
       condition_1 = create(:condition)
       condition_2 = create(:condition)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit conditions_path
 
@@ -17,7 +21,7 @@ describe 'an admin' do
         expect(page).to have_button('Edit')
         expect(page).to have_button('Delete')
       end
-      
+
       expect(page).to have_content(condition_1.date)
       expect(page).to have_content(condition_1.max_temperature_f)
       expect(page).to have_content(condition_1.mean_temperature_f)
