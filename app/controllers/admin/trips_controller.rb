b2e24b0c50146b017db5d93501ef914c6e11e481
Eliot Swank <eliotswank@gmail.com>
2018-07-23 23:50:25 -0600

    adds update method to controller

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,7 +1,16 @@
 class Admin::TripsController < Admin::BaseController
 
   def update
-    
+    @trip = Trip.find(params[:id])
+    old_trip_name = @trip.id
+    @trip.update(trip_params)
+    if @trip.save
+      flash[:notice] = "#{old_trip_name} has been updated!"
+      redirect_to trip_path(@trip)
+    else
+      flash[:notice] = "#{old_trip_name} has not been updated!"
+      render :edit
+    end
   end
 
   def edit
