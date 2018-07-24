a8403950b8968f176fc442a32f31c02a33ba6bed
Eliot Swank <eliotswank@gmail.com>
2018-07-23 23:54:57 -0600

    adds trip params private method in controller

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -16,4 +16,20 @@ class Admin::TripsController < Admin::BaseController
   def edit
     @trip = Trip.find(params[:id])
   end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:duration, 
+                                 :start_date,
+                                 :start_station_name,
+                                 :start_station_id,
+                                 :end_date,
+                                 :end_station_name,
+                                 :end_station_id,
+                                 :bike_id,
+                                 :subscription_type,
+                                 :zip_code
+                                 )
+  end
 end
