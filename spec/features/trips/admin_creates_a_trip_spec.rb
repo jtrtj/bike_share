841ac97b21d03856474bbde0fe9a0aced19292b6
Eliot Swank <eliotswank@gmail.com>
2018-07-24 00:59:50 -0600

    fixes test

--- spec/features/trips/admin_creates_a_trip_spec.rb
+++ spec/features/trips/admin_creates_a_trip_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe "An admin user" do
   context "visits admin trip new" do
     it "fills out a for with all attributes, clicks 'Create Trip' and is redirected to the trip show page" do
+      station = create(:station)
       admin = create(:user, role: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
@@ -13,18 +14,18 @@ describe "An admin user" do
 
       fill_in :trip_duration,	with: 120
       fill_in :trip_start_date,	with: Date.today 
-      fill_in :trip_start_station_name,	with: "Station 1"
-      fill_in :trip_start_station_id,	with: 5
+      fill_in :trip_start_station_name,	with: station.name
+      fill_in :trip_start_station_id,	with: station.id
       fill_in :trip_end_date,	with: Date.today
-      fill_in :trip_end_station_name,	with: "Station 2"
-      fill_in :trip_end_station_id,	with: 10
+      fill_in :trip_end_station_name,	with: station.name
+      fill_in :trip_end_station_id,	with: station.id
       fill_in :trip_bike_id, with: 15
       fill_in :trip_subscription_type,	with: "Subscriber"
       fill_in :trip_zip_code,	with: 91112
 
       click_on "Create Trip"
 
-      expect(current_path).to eq(trip_path(Trip.all.last.id))
+      expect(current_path).to eq(trip_path(Trip.last))
       expect(page).to have_content(120)
       expect(page).to have_content(Date.today)
       expect(page).to have_content("Station 1")
