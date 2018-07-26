03d8cf352757aedc3a098cdad0b832a30fb31ab5
ClaireBvs <clairembeauvais@gmail.com>
2018-07-26 14:23:51 -0600

    Add feature test for visitor item show page

---
+++ spec/features/items/visitor_sees_accessory_show_page_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'A visitor' do
+  describe 'visiting accessory show page' do
+    before :each do
+      item = create(:item, title: 'bike lights', description: 'wonderful led', price: 10, image: 'bike.com')
+    end
+    it 'sees all attributes of this accessory and can click to add it to cart' do
+
+      visit accessory_show(item)
+
+      expect(page).to have_content(item.title)
+      expect(page).to have_content(item.description)
+      expect(page).to have_content(item.price)
+      expect(page).to have_content(item.image)
+      expect(page).to have_link('Add to Cart')
+    end
+  end
+end
