bcc005dad41f576c433b40bb66cf0ead6a56c7d8
ClaireBvs <clairembeauvais@gmail.com>
2018-07-26 15:45:06 -0600

    visitor visits item show page and can add to cart

--- spec/features/items/visitor_sees_accessory_show_page_spec.rb
+++ spec/features/items/visitor_sees_accessory_show_page_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 describe 'A visitor' do
   describe 'visiting accessory show page' do
     before :each do
-      @item = Item.create(title: 'bike lights', description: 'wonderful led', price: 10, image: 'bike.com')
+      @item = Item.create(title: 'bike lights', description: 'wonderful led', price: 10, image: 'https://www.elegantthemes.com/blog/wp-content/uploads/2015/02/custom-trackable-short-url-feature.png')
     end
     it 'sees all attributes of this accessory and can click to add it to cart' do
 
       visit item_path(@item)
 
-      expect(page).to have_content(item.title)
-      expect(page).to have_content(item.description)
-      expect(page).to have_content(item.price)
-      expect(page).to have_content(item.image)
-      expect(page).to have_link('Add to Cart')
+      expect(page).to have_content(@item.title)
+      expect(page).to have_content(@item.description)
+      expect(page).to have_content(@item.price)
+      expect(page).to have_button('Add to Cart')
     end
   end
 end
