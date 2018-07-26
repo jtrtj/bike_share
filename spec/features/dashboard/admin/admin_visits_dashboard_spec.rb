9452910aaf8776c453364f1b088544874867ec4f
ClaireBvs <clairembeauvais@gmail.com>
2018-07-26 16:27:03 -0600

    add feature test to admin view bike shop all accessories

--- spec/features/dashboard/admin/admin_visits_dashboard_spec.rb
+++ spec/features/dashboard/admin/admin_visits_dashboard_spec.rb
@@ -4,8 +4,8 @@ describe 'An admin' do
   describe 'visiting the dashboard' do
     before :each do
       @admin = create(:user, role: 1)
-      @item = Item.create(title: 'bike lights', description: 'wonderful led', price: 10, image: 'https://www.elegantthemes.com/blog/wp-content/uploads/2015/02/custom-trackable-short-url-feature.png')
-      @item2 = Item.create(title: 'wheels', description: 'circle', price: 90, image: 'https://www.elegantthemes.com/blog/wp-content/uploads/2015/02/custom-trackable-short-url-feature.png')
+      @item = Item.create(title: 'bike lights', description: 'wonderful led', price: 10, image: 'https://www.elegantthemes.com/blog/wp-content/uploads/2015/02/custom-trackable-short-url-feature.png', status: 1)
+      @item2 = Item.create(title: 'wheels', description: 'circle', price: 90, image: 'https://www.elegantthemes.com/blog/wp-content/uploads/2015/02/custom-trackable-short-url-feature.png', status: 0)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
     end
     it 'sees link to view all items and click on it current path should be "/admin/bike-shop"' do
@@ -16,24 +16,18 @@ describe 'An admin' do
       click_link 'All Items'
       expect(current_path).to eq(admin_bike_shop_path)
     end
-    it '' do
+    it 'can see all accessories in a table on admin/bike-shop with a thumbnail of the image, description, status, ability to edit accessory and ability to Retire/Reactivate accessory' do
+      visit admin_bike_shop_path
 
+      expect(page).to have_content(@item.description)
+      expect(page).to have_content(@item.title)
+      expect(page).to have_content(@item.price)
+      expect(page).to have_content(@item.status)
+
+      expect(page).to have_content(@item2.description)
+      expect(page).to have_content(@item2.title)
+      expect(page).to have_content(@item2.price)
+      expect(page).to have_content(@item2.status)
     end
   end
 end
-=begin
-As an admin user,
-When I visit "/admin/dashboard",
-I see a link for viewing all accessories,
-When I click that link,
-My current path should be "/admin/bike-shop",
-I see a table with all accessories (active and inactive)
-
-Each accessory should have:
-
-A thumbnail of the image
-Description
-Status
-Ability to Edit accessory
-Ability to Retire/Reactivate accessory
-=end
