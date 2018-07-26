8d44fad377a68c11505be545dea79f0c72b2ea95
Eliot Swank <eliotswank@gmail.com>
2018-07-25 23:50:43 -0600

    updates user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
+require 'faker'
 FactoryBot.define do
   factory :user do
     sequence(:user_name) { |n| "user-#{n}"}
     password 'password'
-    
+    first_name Faker::GreekPhilosophers.name
+    last_name Faker::Name.last_name
+    street_address Faker::Address.street_address
+    city Faker::Address.city
+    state Faker::Address.state
+    zip Faker::Address.zip
     role 0
   end
 end
