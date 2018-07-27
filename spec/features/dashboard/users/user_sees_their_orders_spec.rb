066406d2da0d0b20688e503b0b46327ed6c31e57
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-27 17:45:32 -0600

    Test made for order show page

--- spec/features/dashboard/users/user_sees_their_orders_spec.rb
+++ spec/features/dashboard/users/user_sees_their_orders_spec.rb
@@ -7,23 +7,34 @@ describe "A user" do
     @item_2 = Item.create(title: "Boldfish", description: 'Whoopity doop', image: 'http://via.placeholder.com/100x100', price: 400, status: 1)
     @item_3 = Item.create(title: "Roldfish", description: 'Whoopity loop', image: 'http://via.placeholder.com/100x100', price: 400, status: 1)
 
+    visit '/bike-shop'
+
+    within "#item-#{@item_1.id}" do
+      click_on 'Add to Cart'
+    end
+    within "#item-#{@item_2.id}" do
+      click_on 'Add to Cart'
+    end
+    within "#item-#{@item_3.id}" do
+      click_on 'Add to Cart'
+    end
+
+    visit cart_path
+
+    click_on 'Checkout'
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
   context "logs in" do
     it 'sees all orders' do
-      visit '/bike-shop'
+      expected_number = 1
 
-      within "#item-#{@item_1.id}" do
-        click_on 'Add to Cart'
-      end
-      within "#item-#{@item_2.id}" do
-        click_on 'Add to Cart'
-      end
+      expect(page).to have_content(expected_number)
+    end
+    it 'can click on an order and view order information' do
+      expected_number = 1
 
-      visit cart_path
-
-      click_on 'Checkout'
-
+      expect(page).to have_content(expected_number)
     end
   end
 end
