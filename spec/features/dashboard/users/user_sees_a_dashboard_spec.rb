9f808974fe9bbcc63b218b545f6051d03c3c1c98
Eliot Swank <eliotswank@gmail.com>
2018-07-26 20:10:34 -0600

    adds controller @user method

--- spec/features/dashboard/users/user_sees_a_dashboard_spec.rb
+++ spec/features/dashboard/users/user_sees_a_dashboard_spec.rb
@@ -20,7 +20,7 @@ describe "A user" do
 
     it 'sees profile information' do
       user = create(:user)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit dashboard_path
 
