18edfff163b3f6267d008b0657b9b0d4eb20631d
Eliot Swank <eliotswank@gmail.com>
2018-07-26 19:55:00 -0600

    writes test for user dashboard

---
+++ spec/features/dashboard/users/user_sees_a_dashboard_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe "A user" do
+  context "logs in" do
+    it "and sees 'Logged in as [user name]" do
+      user = create(:user)
+
+      visit '/'
+
+      click_on "Log In"
+
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_content "Logged in as #{user}"
+    end
+  end
+end
+
+=begin
+As a registered user,
+When I visit "/",
+I see a link for "Login",
+
+When I click "Login",
+I should be on the "/login" page,
+I see a place to insert my credentials to login,
+I fill in my desired credentials,
+I submit my information,
+My current page should be "/dashboard",
+I see a message in the navbar that says "Logged in as SOME_USER",
+I see my profile information,
+I do not see a link for "Login",
+I see a link for "Logout".
+=end
