f8273de6827e952c19499c0def792dee6573e3ee
jtrtj <jt.r@live.com>
2018-07-26 18:32:12 -0600

    start making create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,13 @@
 class OrdersController < ApplicationController
   def create
-    
+    user = current_user
+    user.orders.create(order_params)
     redirect_to dashboard_path
+  end
+
+  private
+
+  def order_params
+    params.require(:cart_contents)
   end
 end
