51f9ca956db087b3b40db6ba7071d33add3843e0
jtrtj <jt.r@live.com>
2018-07-26 18:48:25 -0600

    write generate order items model test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,4 +9,22 @@ describe Order, type: :model do
     it {should have_many(:order_items)}
     it {should belong_to(:user)}
   end
+  context 'class methods' do
+    it '#generate_order_items' do
+      user = create(:user)
+      item_1 = Item.create(title: "Goldfish", description: 'Whoopity scoop', image: 'http://via.placeholder.com/100x100', price: 400, status: 1)
+      item_2 = Item.create(title: "Boldfish", description: 'Whoopity doop', image: 'http://via.placeholder.com/100x100', price: 400, status: 1)
+      order = user.orders.create
+
+      cart = {item_1.id.to_s => 1, item_2.id.to_s => 2}
+      order.generate_order_items(cart)
+
+
+      expect(order.order_items[0].item_id).to eq(item_1.id)
+      expect(order.order_items[1].item_id).to eq(item_2.id)
+      
+      expect(order.order_items[0].quantity.to eq(1)
+      expect(order.order_items[1].quantity.to eq(2)     
+    end
+  end
 end
