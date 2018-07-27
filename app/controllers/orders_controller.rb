4e0411e0cf6c2edc3b640d0684d0cdbffae15804
jtrtj <jt.r@live.com>
2018-07-26 18:41:33 -0600

    fully build order create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,8 @@
 class OrdersController < ApplicationController
   def create
     user = current_user
-    user.orders.create(order_params)
+    order = user.orders.create
+    order.generate_order_items(session[:cart])
     redirect_to dashboard_path
-  end
-
-  private
-
-  def order_params
-    params.require(:cart_contents)
   end
 end
