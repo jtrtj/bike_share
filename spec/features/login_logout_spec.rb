67ae764430ef4e3f9e5a9f2847fa5f423097631f
ClaireBvs <clairembeauvais@gmail.com>
2018-07-28 14:12:46 -0600

    add test for log in as admin

--- spec/features/login_logout_spec.rb
+++ spec/features/login_logout_spec.rb
@@ -40,3 +40,27 @@ describe 'a registered user' do
     end
   end
 end
+
+describe 'an admin' do
+  before(:each) do
+    @admin = create(:user, role: 1)
+  end
+  context 'logs in to their account' do
+    it 'has a functional username and password' do
+      visit root_path
+
+      click_link 'Log In'
+      expect(current_path).to eq(login_path)
+      expect(page).to have_button('Login')
+
+      fill_in :user_name, with: @admin.user_name
+      fill_in :password, with: @admin.password
+      click_on 'Login'
+
+      expect(current_path).to eq(admin_dashboard_path)
+      expect(page).to have_content("Logged in as Admin User: #{@admin.user_name}")
+      expect(page).to have_content('Log Out')
+      expect(page).to_not have_content('Login')
+    end
+  end
+end
