c7b8a8fb76e76955669a989bc1ef975914212494
Eliot Swank <eliotswank@gmail.com>
2018-07-28 17:02:13 -0600

    add test for user edit

---
+++ spec/features/dashboard/users/user_can_edit_personal_info_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "A user" do
+  context "visits the user edit page" do
+    it "fills out a form and updates user infomation" do
+      new_address = "2411 12th Ave Ct"
+      new_city = "Greeley"
+      new_zip = 80631
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit edit_user_path(user)
+
+      fill_in :user_street_address,	with: new_address
+      fill_in :user_city,	with: new_city
+      fill_in :user_zip,	with: new_zip
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content(new_address) 
+      expect(page).to have_content(new_city) 
+      expect(page).to have_content(new_zip) 
+    end
+  end
+end
