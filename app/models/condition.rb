3a946e6ad6aa89cb8a5d8c4094d3320cd80ad124
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-29 10:49:59 -0600

    all tests for database calls in conditions dashboard passing

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -10,7 +10,22 @@ class Condition < ApplicationRecord
   validates_presence_of :zip_code
 
   def self.trip_numbers_by_temp_range
-    wuh = joins('INNER JOIN trips ON conditions.date = trips.start_date')
+    joins('INNER JOIN trips ON conditions.date = trips.start_date')
     .group(:date, :max_temperature_f).count
+  end
+
+  def self.trip_numbers_by_precipitation
+    joins('INNER JOIN trips ON conditions.date = trips.start_date')
+    .group(:date, :precipitation_inches).count
+  end
+
+  def self.trip_numbers_by_wind_speed
+    joins('INNER JOIN trips ON trips.start_date = conditions.date')
+    .group(:date, :mean_wind_speed_mph).count
+  end
+
+  def self.trip_numbers_by_visibility
+    joins('INNER JOIN trips ON trips.start_date = conditions.date')
+    .group(:date, :mean_visibility_miles).count
   end
 end
