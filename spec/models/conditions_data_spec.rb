7928818f308855a45a41f4795f2a5f25580dfdb8
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-29 13:52:09 -0600

    tests for mean visibility by rides complete

--- spec/models/conditions_data_spec.rb
+++ spec/models/conditions_data_spec.rb
@@ -10,13 +10,13 @@ describe ConditionsData do
       @condition_2 = create(:condition, date: Date.strptime('8/16/2015 16:45', '%m/%d/%Y'),
       max_temperature_f: 91, precipitation_inches: 3.1, mean_wind_speed_mph: 21.0, mean_visibility_miles: 21.0)
       @condition_3 = create(:condition, date: Date.strptime('8/17/2015 16:45', '%m/%d/%Y'),
-      max_temperature_f: 87, precipitation_inches: 2.8, mean_wind_speed_mph: 19.0, mean_visibility_miles: 19.0)
+      max_temperature_f: 87, precipitation_inches: 2.8, mean_wind_speed_mph: 19.0, mean_visibility_miles: 18.0)
       @condition_4 = create(:condition, date: Date.strptime('8/18/2015 16:45', '%m/%d/%Y'),
-      max_temperature_f: 86, precipitation_inches: 2.6, mean_wind_speed_mph: 18.0, mean_visibility_miles: 18.0)
+      max_temperature_f: 86, precipitation_inches: 2.6, mean_wind_speed_mph: 18.0, mean_visibility_miles: 17.0)
       @condition_5 = create(:condition, date: Date.strptime('8/19/2015 16:45', '%m/%d/%Y'),
-      max_temperature_f: 74, precipitation_inches: 2.4, mean_wind_speed_mph: 14.0, mean_visibility_miles: 14.0)
+      max_temperature_f: 74, precipitation_inches: 2.4, mean_wind_speed_mph: 14.0, mean_visibility_miles: 15.0)
       @condition_6 = create(:condition, date: Date.strptime('8/20/2015 16:45', '%m/%d/%Y'),
-      max_temperature_f: 72, precipitation_inches: 2.3, mean_wind_speed_mph: 13.0, mean_visibility_miles: 13.0)
+      max_temperature_f: 72, precipitation_inches: 2.3, mean_wind_speed_mph: 13.0, mean_visibility_miles: 12.0)
       @trip_1 = create(:trip, start_date: Date.strptime('8/15/2015 16:45', '%m/%d/%Y'), start_station_id: 25, end_station_id: 56)
       @trip_2 = create(:trip, start_date: Date.strptime('8/15/2015 16:45', '%m/%d/%Y'), start_station_id: 25, end_station_id: 56)
       @trip_3 = create(:trip, start_date: Date.strptime('8/16/2015 16:45', '%m/%d/%Y'), start_station_id: 25, end_station_id: 56)
@@ -62,6 +62,7 @@ describe ConditionsData do
                                                                              1.5]
                                                                 })
     end
+
     it '#max_rides_by_precipitation(range)' do
       data = ConditionsData.new(Condition.trip_numbers_by_precipitation)
       range = (3.0)
@@ -99,6 +100,7 @@ describe ConditionsData do
                                                                           1.5]
                                                             })
     end
+
     it '#max_rides_by_wind_speed(range)' do
       data = ConditionsData.new(Condition.trip_numbers_by_wind_speed)
       range = (20.0)
@@ -137,6 +139,42 @@ describe ConditionsData do
                                                             })
     end
 
+    it '#max_rides_by_visibility(range)' do
+      data = ConditionsData.new(Condition.trip_numbers_by_visibility)
+      range = (20.0)
+      expected_number = 2
 
+      expect(data.most_rides_by_visibility(range).keys[0]).to eq(expected_number)
+      expect(data.most_rides_by_visibility(range).values[0][0]).to eq(@condition_1.date)
+    end
+    xit '#min_rides_by_visibility(range)' do
+      data = ConditionsData.new(Condition.trip_numbers_by_visibility)
+      range = (20.0)
+      expected_number = 1
+
+      expect(data.least_rides_by_visibility(range).keys[0]).to eq(expected_number)
+      expect(data.least_rides_by_visibility(range).values[0][0]).to eq(@condition_2.date)
+    end
+    xit '#avg_rides_by_visibility(range)' do
+      data = ConditionsData.new(Condition.trip_numbers_by_visibility)
+      range = (20.0)
+
+      expect(data.average_rides_by_visibility(range)).to eq(1.5)
+    end
+    xit '#all_rides_by_visibility(range)' do
+      data = ConditionsData.new(Condition.trip_numbers_by_visibility)
+      range = (12.0..20.0)
+
+      expect(data.all_rides_by_visibility(range)).to eq({20.0 => [{2 => [@condition_1.date, 23.0]},
+                                                                          {1 => [@condition_2.date, 21.0]},
+                                                                          1.5],
+                                                            16.0 => [{2 => [@condition_3.date, 19.0]},
+                                                                          {1 => [@condition_4.date, 18.0]},
+                                                                          1.5],
+                                                            12.0 => [{2 => [@condition_5.date, 14.0]},
+                                                                          {1 => [@condition_6.date, 13.0]},
+                                                                          1.5]
+                                                            })
+    end
   end
 end
