16a82f0836527258c5151c4e4a329a9c9c2861bf
jtrtj <jt.r@live.com>
2018-07-29 14:00:48 -0600

    add admin check tests and pass them immediately

--- spec/features/user_permissions_spec.rb
+++ spec/features/user_permissions_spec.rb
@@ -15,4 +15,33 @@ describe 'a registered user' do
       expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
+
+  context 'trying to use admin screens and fucntions' do
+    it 'cannot view admin screens or use admin functionality' do
+      visit admin_dashboard_index_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+
+      visit admin_bike_shop_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+
+      visit new_admin_item_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+
+      visit new_admin_trip_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+
+      visit new_admin_condition_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+
+      visit new_admin_station_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+
+    it 'cannot sign up as an admin' do
+      visit new_user_path
+
+      expect(page).to_not have_content("admin")
+      expect(page).to_not have_content("Admin")
+    end
+  end
 end
