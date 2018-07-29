4a6dad9e64fd50323994e4ebe66e5cb51f2c85d0
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-29 14:08:59 -0600

    feature tests for conditions dashboard complete

--- spec/features/dashboard/conditions/registered_user_visits_condition_dashboard_spec.rb
+++ spec/features/dashboard/conditions/registered_user_visits_condition_dashboard_spec.rb
@@ -6,15 +6,21 @@ I see the Breakout of average number of rides, highest number of rides, and lowe
 
 describe 'A registered user' do
   context 'visiting dashboard' do
-    before :each do
+    before(:each) do
       @station_1 = create(:station, id: 25)
       @station_2 = create(:station, id: 56)
-      @condition_1 = create(:condition, date: Date.strptime('8/15/2015 16:45', '%m/%d/%Y'), max_temperature_f: 93)
-      @condition_2 = create(:condition, date: Date.strptime('8/16/2015 16:45', '%m/%d/%Y'), max_temperature_f: 91)
-      @condition_3 = create(:condition, date: Date.strptime('8/17/2015 16:45', '%m/%d/%Y'), max_temperature_f: 87)
-      @condition_4 = create(:condition, date: Date.strptime('8/18/2015 16:45', '%m/%d/%Y'), max_temperature_f: 86)
-      @condition_5 = create(:condition, date: Date.strptime('8/19/2015 16:45', '%m/%d/%Y'), max_temperature_f: 74)
-      @condition_6 = create(:condition, date: Date.strptime('8/20/2015 16:45', '%m/%d/%Y'), max_temperature_f: 72)
+      @condition_1 = create(:condition, date: Date.strptime('8/15/2015 16:45', '%m/%d/%Y'),
+      max_temperature_f: 93, precipitation_inches: 3.2, mean_wind_speed_mph: 23.0, mean_visibility_miles: 22.0)
+      @condition_2 = create(:condition, date: Date.strptime('8/16/2015 16:45', '%m/%d/%Y'),
+      max_temperature_f: 91, precipitation_inches: 3.1, mean_wind_speed_mph: 21.0, mean_visibility_miles: 21.0)
+      @condition_3 = create(:condition, date: Date.strptime('8/17/2015 16:45', '%m/%d/%Y'),
+      max_temperature_f: 87, precipitation_inches: 2.8, mean_wind_speed_mph: 19.0, mean_visibility_miles: 18.0)
+      @condition_4 = create(:condition, date: Date.strptime('8/18/2015 16:45', '%m/%d/%Y'),
+      max_temperature_f: 86, precipitation_inches: 2.6, mean_wind_speed_mph: 18.0, mean_visibility_miles: 17.0)
+      @condition_5 = create(:condition, date: Date.strptime('8/19/2015 16:45', '%m/%d/%Y'),
+      max_temperature_f: 74, precipitation_inches: 2.4, mean_wind_speed_mph: 14.0, mean_visibility_miles: 15.0)
+      @condition_6 = create(:condition, date: Date.strptime('8/20/2015 16:45', '%m/%d/%Y'),
+      max_temperature_f: 72, precipitation_inches: 2.3, mean_wind_speed_mph: 13.0, mean_visibility_miles: 12.0)
       @trip_1 = create(:trip, start_date: Date.strptime('8/15/2015 16:45', '%m/%d/%Y'), start_station_id: 25, end_station_id: 56)
       @trip_2 = create(:trip, start_date: Date.strptime('8/15/2015 16:45', '%m/%d/%Y'), start_station_id: 25, end_station_id: 56)
       @trip_3 = create(:trip, start_date: Date.strptime('8/16/2015 16:45', '%m/%d/%Y'), start_station_id: 25, end_station_id: 56)
@@ -40,5 +46,14 @@ describe 'A registered user' do
       expect(page).to have_content("Most rides for 90 degree weather: #{expected_max_90}, on #{expected_date_1.to_s}, at #{expected_temp_1}")
       expect(page).to have_content("Least rides for 90 degree weather: #{expected_min_90}, on #{expected_date_2.to_s}, at #{expected_temp_2}")
     end
+    it 'sees average, highest and lowest number of rides for precipitation for increments of 0.5 inches' do
+      visit conditions_dashboard_path
+
+      expect(page).to have_content("Most rides: #{@condition_1.precipitation_inches}")
+      expect(page).to have_content("Least rides: #{@condition_2.precipitation_inches}")
+      expect(page).to have_content("Most rides: #{@condition_1.mean_wind_speed_mph}")
+      expect(page).to have_content("Least rides: #{@condition_2.mean_wind_speed_mph}")
+      expect(page).to have_content("Most rides: #{@condition_1.mean_visibility_miles}")
+      expect(page).to have_content("Least rides: #{@condition_2.mean_visibility_miles}")
   end
 end
