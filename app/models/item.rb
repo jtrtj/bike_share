fbd680aceff591e0468f35b355487cfe721d9658
jtrtj <jt.r@live.com>
2018-07-30 11:03:53 -0600

    update item to not accept negative or zero values

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   validates_presence_of :title, :description, :image, :price, :status
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 
   enum status: %w(retired active)
 end
